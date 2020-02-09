c78d1e9ee633e9c27d1c3db0af35d8b1fb7359a0
Gustavo Bauke <baukegustavo@gmail.com>
2020-02-09 17:08:09 -0300

    First commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,76 @@
+import './../../styles/pages/Main.css';
+
+import React, { useEffect, useState } from 'react';
+
+import api from './../../services/api';
+
+export default function Main() {
+  const [title, setTitle] = useState('');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  }, [items]);
+
+  async function getItems() {
+    const response = await api.get('/entries');
+
+    if(response.status === 200) {
+      setItems(response.data);
+    }
+  }
+
+  async function addtem(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+    }
+    const response = await api.post('/entries', data);
+
+    if(response.status === 200) {
+      setItems([...items, response.data]);
+      setTitle('');
+    }
+  }
+
+  async function deleteItem(id, index) {
+    await api.delete(`/entries/${id}`);
+
+    let arr = items;
+    arr.splice(index, 1);
+
+    setItems(arr);
+  }
+
+  return (
+    <div className="container">
+      {items.length !== 0 && <ul className="download-list">
+        {items.map((item, index) => {
+          return (
+            <li>
+              <h3>{item.title}</h3>
+              <span onClick={() => deleteItem(item.id, index)}>
+                Delete
+              </span>
+            </li>
+          );
+        })}
+      </ul>}
+      
+      <form className="add-item-form" onSubmit={addtem}>
+        <div className="input">
+          <label>Title</label>
+          <input 
+            type="text" 
+            className="title-input" 
+            autoCapitalize="words" 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add to List</button>
+      </form>
+    </div>
+  )
+}
